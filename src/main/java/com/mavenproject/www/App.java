9f1b14df6fda84affbc8a5b02590f0abf7eba477
root <root@ip-172-31-16-131.us-west-1.compute.internal>
2022-06-04 05:14:23 +0000

    new java project!!!!!

---
+++ src/main/java/com/mavenproject/www/App.java
@@ -0,0 +1,13 @@
+package com.mavenproject.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bhargavi Good Morning!!!!!!!!!!11!" );
+    }
+}
